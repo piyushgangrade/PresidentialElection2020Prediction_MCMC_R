fe97dc8d6a6af0c76aff911ecc92a54e446952df
piyushgangrade <piyush0fc@gmail.com>
2024-06-22 17:32:15 -0400

    Redundant files removed

--- data_files/State_wise2016/kansas_trump_vs_clinton-5904_files/footer_info_links.js
+++
@@ -1,8 +0,0 @@
-document.write('<ul>'+
-'<li><a href="/about.html">About Us</a></li>'+
-'<li><a href="/privacy.html">Privacy Policy</a></li>'+
-'<li><a href="/media_kit/contact_us.html">Advertise With Us</a></li>'+
-'<li><a href="/contact.html">Contact</a></li>'+
-'<li><a href="/rss">RSS</a></li>'+
-'<li class="view-mobile"><a onClick="loadMobile();">View Mobile Site</a></li>'+
-'</ul><div class="clear"></div><center><a href="/copyright.html" class="copyright-button">Copyright &copy; '+(new Date().getFullYear())+' RealClearHoldings, LLC.</a> <br/><a href="javascript:void(0)" class="ccpaOptOut" style="display:none; padding:10px 0 0 0; cursor:pointer;">Do Not Sell My Personal Information</a></center>');
